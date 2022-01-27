00c1e39dec57dfbf4ec7901cb5bb8ef58a7234ed
mohit <themohit002@gmail.com>
2022-01-28 02:40:15 +0530

    first commit

---
+++ Ques2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int salary;
+cout<<"\n enter salary ::";
+cin>>salary;
+
+if(salary >=3000)
+{
+    cout<<"\n you are rich";
+}
+
+else if (salary <=3000)
+{
+    cout<<"\n you are poor";
+}
+else 
+{
+    cout<<"\n all ok";
+}
+
+return 0;
+}
